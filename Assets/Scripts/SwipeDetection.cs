18e6bad81e0d1c29aa50d9b00a70763771e1d279
Ran Zonis <zonisr@gmail.com>
2022-08-08 17:28:15 +0300

    directions resolved from swipe

---
+++ Assets/Scripts/SwipeDetection.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Direction
+{
+    Up, Down, Right, Left, Error
+}
+
+public class SwipeDetection : MonoBehaviour
+{
+    private Vector2 _swipeStartPosition;
+    private Vector2 _swipeEndPosition;
+    public Direction SwipeDirection;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void OnMouseDown()
+    {
+        _swipeStartPosition = Input.mousePosition;
+    }
+
+    private void OnMouseUp()
+    {
+        _swipeEndPosition = Input.mousePosition;
+        Debug.Log(AngleToDireciton());
+        //Debug.Log(GetSwipeAngle());
+        //Debug.Log("End " + _swipeEndPosition);
+        //Debug.Log("Start " + _swipeStartPosition);
+    }
+
+    private float GetSwipeAngle()
+    {
+        float swipeAngle = Vector2.SignedAngle((_swipeEndPosition - _swipeStartPosition), Vector2.up);
+        if (swipeAngle < 0) swipeAngle += 360;
+        return swipeAngle;
+    }
+
+    private Direction AngleToDireciton()
+    {
+        float angle = GetSwipeAngle();
+
+        if (angle is > 0 and < 90)
+            return Direction.Up;
+        else if (angle is > 90 and < 180)
+            return Direction.Right;
+        else if (angle is > 180 and < 270)
+            return Direction.Down;
+        else if (angle is > 270 and < 360)
+            return Direction.Left;
+
+        return Direction.Error;
+    } 
+}

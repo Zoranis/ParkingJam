12f77f3b31100039aaef437cfc71684758888700
Ran Zonis <zonisr@gmail.com>
2022-08-10 22:04:00 +0300

    web gl build works

--- Assets/Scripts/CarMove.cs
+++ Assets/Scripts/CarMove.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Runtime.CompilerServices;
-using UnityEditor.UIElements;
 using UnityEngine;
 
 public enum CarAlignment
